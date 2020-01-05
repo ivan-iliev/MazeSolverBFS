501168492d8b85139918d1bf0aa55bf443da5a43
Ivan Iliev <mr.i.iliev@gmail.com>
2020-01-05 16:24:35 +0200

    Update main.js

--- MazeSolverV2/js/main.js
+++ MazeSolverV2/js/main.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
                 });
                 console.log(pointerEventToXY(e)); 
             
+                
                 if(gg.isStart())
                 {
                     $(this).unbind();
